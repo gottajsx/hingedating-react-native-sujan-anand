350ea9786392bae0d62df634f3ef14862ee61ce2
gottajsx <gottajsx@gmail.com>
2024-09-01 21:58:32 +0200

    Adding LikeScreen.js

---
+++ screens/Likesscreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Likesscreen = () => {
+  return (
+    <View>
+      <Text>Likesscreen</Text>
+    </View>
+  )
+}
+
+export default Likesscreen
+
+const styles = StyleSheet.create({})
